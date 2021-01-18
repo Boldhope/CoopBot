47bb62d1d8435cb638d3cea25e311d8a54cc09ae
Boldninja <brianluu@prodigy.net>
2021-01-18 18:05:23 +0000

    Add dictionary dispatcher method for user input. Fix newline bug with removecoopgame command.

--- executeCommands.py
+++ executeCommands.py
@@ -1,6 +1,6 @@
 #--------------------------------------------------------
 # executecommands.py
-# What it does: executes the commands for what is called by the user in main.py
+# What it does: Executes the commands for what is called by the user in main.py. Currently, they are encapsulated in generic methods that are defined by the bot dictionary in main.py. 
 # Dependencies: enum, discord, os, commandsupport.py, monitorProcess.py,
 #Notes:TO DO: MAKE TASKS OUT OF THE SCHEDULED EVENTS. PASS THEM IN HERE, AND CHECK IF THERE ARE ANY FOR A SPECIFIC GAME AND PREVENT   #REMOVAL IF THERE IS. For removeGame
 #TO DO: USE STEAM STORE PAGE AND TRACK DOWN GAMES ACCORDING TO THEIR TAGS, LIKE "COOP", "ACTION", ETC. for findGames
@@ -13,8 +13,8 @@ from monitorProcess import *
 
 
 #Open the games.txt file, and write out the list of coop games available.
-async def listGames(discordChannel, fileName):
-  f = open(fileName)
+async def listGames(discordChannel, discordUser, scheduleInstance, args, gameFile, tempFileName):
+  f = open(gameFile)
   #Read each line in f for the game list into a buffer, and write out as one message
   buffer = "The list of games are as follows:\n"
   buffer += "```"
@@ -25,12 +25,13 @@ async def listGames(discordChannel, fileName):
   await discordChannel.send(buffer)
 
 #Determine whether game exists. If so, let user know it already exists. Else, display confirmation and add to the games.txt file
-async def addGame(discordChannel, gameName, fileName):
+async def addGame(discordChannel, discordUser, scheduleInstance, args, gameFile, tempFileName):
   
   #Declare/Initialize variables for use.
+  gameName = getUserInputtedGameName(args)
   gameList = []
   #Fill out our game list
-  f = open(fileName)
+  f = open(gameFile)
   for line in f:
     tempstr = line.split('\n')
     gameList.append(tempstr[0])
@@ -40,35 +41,40 @@ async def addGame(discordChannel, gameName, fileName):
   if(gameName in gameList):
     await discordChannel.send("This game is already in the list")
   else:
-    e = open(fileName, "a")
+    e = open(gameFile, "a")
     e.write("\n" + gameName)
     await discordChannel.send(gameName + " has been added to the list.")
     e.close()
   f.close()
 
 #List the schedules available, which were input by discord users.
-async def listSchedules(discordChannel, scheduleInstance):
+async def listSchedules(discordChannel, discordUser, scheduleInstance, args, gameFile, tempFileName):
   await discordChannel.send(scheduleInstance.giveInfo())
 
 #If there is not already another process waiting to get deleted, then request deletion for a given asynchronous object.
-async def removeSchedule(discordChannel, scheduleInstance, scheduleIdentifier):
+async def removeSchedule(discordChannel, discordUser, scheduleInstance, args, gameFile, tempFileName):
+  scheduleIdentifier = args[0]
   if(scheduleInstance.removeSchedule(scheduleIdentifier)):
     await discordChannel.send("Schedule removal success.")
 
 #Add method for the user to add a game to a particular schedule.
-async def addGametoSchedule(discordChannel, scheduleInstance, scheduleIdentifier, gameName):
+async def addGametoSchedule(discordChannel, discordUser, scheduleInstance, args, gameFile, tempFileName):
+  scheduleIdentifier = args[len(args)-1]
+  args.remove(scheduleIdentifier)
+  gameName = getUserInputtedGameName(args)
   if(scheduleInstance.addGame(scheduleIdentifier, gameName)):
     await discordChannel.send("Game added to the schedule.")
   else:
     await discordChannel.send("Game already exists as a part of the schedule or this schedule does not exist.")
 
 #Add method for the user to add themselves to a particular schedule.
-async def addMembertoSchedule(discordChannel, scheduleInstance, scheduleIdentifier, discordUser):
+async def addMembertoSchedule(discordChannel, discordUser, scheduleInstance, args, gameFile, tempFileName):
+  scheduleIdentifier = args[0]
   scheduleInstance.addMember(scheduleIdentifier, discordUser)
   await discordChannel.send(discordUser.display_name + " has successfully joined the schedule.")
 
 #Specify in terms of day (Mon, Tue, etc...), time (and AM/PM), and timezone.
-async def scheduleTime(discordChannel, args, scheduleInstance):
+async def scheduleTime(discordChannel, discordUser, scheduleInstance, args, gameFile, tempFileName):
   
   #Place user arguments into day, time, am/pm, and timezone.
   #Note there needs to be method that handles the time zone conversion for pytz. It can only handle UTC right now.
@@ -84,33 +90,44 @@ async def scheduleTime(discordChannel, args, scheduleInstance):
 
 
 #Print from the help.txt, which is preformatted with information
-async def listPossibleCommands():
+async def listPossibleCommands(discordChannel, discordUser, scheduleInstance, args, gameFile, tempFileName):
   print("Does Nothing")
 
 #Must be able to prevent removal of a specific game, if there are still scheduler tasks running.
 #Take the following format !removecoopgame <GameName>
-async def removeGame(discordChannel, userInput, fileName, tempfileName):
+async def removeGame(discordChannel, discordUser, scheduleInstance, args, gameFile, tempFileName):
 
+  userInput = getUserInputtedGameName(args)
   removalSuccessful = False
   #Open the respective files for read and write
-  f = open(fileName)
-  e = open(tempfileName, "w")
+  f = open(gameFile)
+  e = open(tempFileName, "w")
 
   #Check the games.txt line by line to find the game we want to remove.
+  linesToAdd = ""
+  lineCount = 0
   for line in f:
+    lineCount += 1
     tempstr = line.split('\n')
-    #Output lines that do not correspond to the game we want to remove. Else, remove that line.
+    #Output lines that do not correspond to the game we want to remove (include a newline character at the beginning of the line if it is not initial line). Else, remove that line.
     if(tempstr[0] == userInput):
       removalSuccessful = True
     else:
-      e.write(line)
+      if(lineCount == 1):
+        e.write(tempstr[0])
+      else:
+        e.write("\n" + tempstr[0])
+  
+  #Write to the temp file.
+  e.write(linesToAdd)
+
   #Close the files.
   f.close()
   e.close()
   
   #Delete the previous file, games.txt, and rename temp.txt to games.txt.
-  os.remove(fileName)
-  os.rename(tempfileName, fileName)
+  os.remove(gameFile)
+  os.rename(tempFileName, gameFile)
   
   if(removalSuccessful):
     await discordChannel.send("Successfully Removed " + userInput)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # main.py
 # What it does: Some other functions which are used from time to time. To support commands.
 # Dependencies: discord, os, executecommands.py, monitorProcess.py, commandsupport.py
+#TO DO: New command that takes in argument membersin # to get which members are part of a specific schedule.
 #--------------------------------------------------------
 import discord 
 import os
@@ -9,8 +10,18 @@ from executeCommands import *
 from monitorProcess import *
 from commandSupport import getUserInputtedGameName
 bot = discord.Client()
-possibleCommands = ["listcoopgames", "addcoopgame", "schedule"]
-possibleCommandDescriptions = ["", "", ""]
+
+botDict = {
+  "help": listPossibleCommands,
+  "listcoopgames": listGames,
+  "addcoopgame": addGame,
+  "removecoopgame": removeGame,
+  "schedule": scheduleTime,
+  "joinschedule": addMembertoSchedule,
+  "addgametoschedule": addGametoSchedule,
+  "listschedule": listSchedules,
+  "removeschedule": removeSchedule,
+}
 
 @bot.event
 async def on_ready():
@@ -45,55 +56,8 @@ async def checkCommand(discordCommand, commandProperties, args):
   #For now, this class is a singleton, which I don't really fancy...it will be passed down from here to reduce its usage elsewhere (for easy removal).
   scheduleInstance = processMonitor.getInstance()
 
-  #List all the possible commands we are able toi
-  if(discordCommand == "help"):
-    await listPossibleCommands()
+  botFunction = botDict[discordCommand]
 
-  #List all coop games which are part of the coop game list. Can be added to.
-  elif(discordCommand == "listcoopgames"):
-    await listGames(discordChannel, gamesFile)
-
-  #Allow user to add a coop game to the list
-  elif(discordCommand == "addcoopgame"):
-    gameName = getUserInputtedGameName(args)
-    await addGame(discordChannel, gameName, gamesFile)
-
-  #Remove a coop game from the list.
-  elif (discordCommand == "removecoopgame"):
-    gameName = getUserInputtedGameName(args)
-    await removeGame(discordChannel, gameName, gamesFile, tempFileName)
-  
-  #Schedule coop game time for reminder
-  #Take in people of group/time to alert
-  elif(discordCommand == "schedule"):
-    await scheduleTime(discordChannel, args, scheduleInstance)
-  
-  #Allow a user to join a particular schedule
-  elif(discordCommand == "joinschedule"):
-    await addMembertoSchedule(discordChannel,scheduleInstance, args[0], discordUser)
-
-  #Allow a user to add a particular game to a schedule.
-  elif(discordCommand == "addgametoschedule"):
-    #Extract schedule ID and remove it from the list of earlier extracted arguments.
-    scheduleID = args[len(args)-1]
-    args.remove(scheduleID)
-    
-    #get full string of user input for the game name.
-    gameName = getUserInputtedGameName(args)
-
-    await addGametoSchedule(discordChannel, scheduleInstance, scheduleID, gameName)
-
-  #List ongoing plans (schedules)
-  elif(discordCommand == "listschedule"):
-    await listSchedules(discordChannel, scheduleInstance)
-  
-  #Remove a schedule from the list
-  elif(discordCommand == "removeschedule"):
-
-    await removeSchedule(discordChannel, scheduleInstance, args[0])
-
-  #Incorrect command was entered. Instruct the user to type !help for more information.
-  else:
-    await channel.send("Incorrect command, try entering !help for a list of commands")
+  await botFunction(discordChannel, discordUser, scheduleInstance, args, gamesFile, tempFileName)
 
 bot.run(os.getenv('TOKEN'))
